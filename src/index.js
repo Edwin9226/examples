da7d91e006dbed773dc93d8dd3dbb19838916408
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-11-08 08:53:10 -0600

    Revert "prueba"
    This reverts commit 6159ac6a23e6b99863abd4effb9d4d03bfc3761d.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -16,4 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
+assd
