168ea7767dca56b2aa0f51fe018a76bbaa9c40ec
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-11-08 09:25:45 -0600

    "Const y let"

---
+++ src/base/01-const-let.js
@@ -0,0 +1,20 @@
+
+console.log("Mensaje de error")
+
+
+// Variables y constantes.
+//var no usado 
+const nombre= 'Edwin';
+const apellido= 'Pinchao';
+
+
+let valorDado= 5;
+valorDado=4;
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    let valorDado= 6;
+    console.log(valorDado);
+    const nombre ='Peter';
+    console.log(nombre);
+}
