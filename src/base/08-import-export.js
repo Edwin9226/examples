41db1e2cd83ea8b1622a335e1fc075231b0c5d37
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-11-09 07:41:28 -0600

    "Create Barrel y Import y export"

---
+++ src/base/08-import-export.js
@@ -0,0 +1,19 @@
+import { heroes } from './data';
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+console.log(heroes);
+
+const getHeroeById=(ids)=>{
+    return heroes.find(({id})=> id === ids);
+}
+
+console.log(getHeroeById(2));
+
+const getHeroeByOwner =(owner1)=>{
+    return heroes.filter(({owner})=> owner === owner1);
+}
+
+console.log(getHeroeByOwner('DC'));
